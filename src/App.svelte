b56a783daf71e6d7cd868382478c44bbaeafeb74
Jose Fernando Resendiz Lopez <gerbo67@outlook.es>
2021-06-23 19:30:57 -0500

    Se declara la base completa junto con la pantalla load

---
+++ src/App.svelte
@@ -0,0 +1,83 @@
+<script>
+    import {onMount} from 'svelte';
+    import LogoWrixy from './components/LogoWrixy.svelte';
+
+    let data = [];
+
+    onMount(async () => {
+        try {
+            const protocolo = window.location.protocol != 'https:' ? 'http' : 'https';
+            let response = await fetch(`${protocolo}://localhost:3000`);
+            response = await response.json();
+            data = response;
+
+            console.log(response);
+        } catch (e) {
+
+        }
+    })
+</script>
+
+<style>
+    :global(:root) {
+        --primary: #B77FDC;
+        --secondary: #F9EBFD;
+        --white: #FFF;
+        --dark-shadow: #00000042;
+        --dark: #AC9DA9AA;
+        --header-elements: 160px;
+        --font-family: 'Mukta', sans-serif;
+
+        --z-back: -10;
+        --z-normal: 1;
+        --z-tooltip: 10;
+        --z-fixed: 100;
+        --z-modal: 1000;
+    }
+
+    main {
+        padding: 0;
+        margin: 0;
+        height: 100%;
+    }
+
+    .loader-container {
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        background: var(--secondary);
+        filter: opacity(.9);
+        color: var(--primary);
+    }
+
+    .loader {
+        animation: loader 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+        width: 2.5vw;
+        height: 2.5vw;
+        min-width: 40px;
+        min-height: 40px;
+    }
+
+    @keyframes loader {
+        0% {
+            transform: scale(4);
+        }
+        50% {
+            transform: scale(3);
+            filter: opacity(0.7);
+        }
+        100% {
+            transform: scale(4);
+        }
+    }
+</style>
+
+<main>
+    <div class="loader-container">
+        <div class="loader">
+            <LogoWrixy/>
+        </div>
+    </div>
+</main>
---
+++ src/components/HeaderG.svelte
@@ -0,0 +1,190 @@
+<script>
+    const showMenu = () => {
+        const nav = document.getElementById('main-nav');
+        if (nav.style.right === '0vw') {
+            nav.style.right = '-80vw';
+        } else {
+            nav.style.right = '0vw';
+        }
+    }
+
+</script>
+
+<style>
+    header {
+        z-index: var(--z-fixed);
+        width: 100%;
+        height: 4.8rem;
+        background: var(--white);
+        border-radius: 0 0 2rem 2rem;
+        display: flex;
+        box-shadow: 0 4px 4px var(--dark-shadow);
+    }
+
+    .header-elements {
+        width: 100%;
+        margin: 2.25rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .header-logo__img {
+        width: 160px;
+        height: auto;
+    }
+
+    .header-search {
+        display: flex;
+        justify-content: center;
+        color: var(--primary);
+        height: calc(var(--header-elements) * 0.25);
+        background-color: var(--secondary);
+        border-radius: 10px;
+    }
+
+    .header-search__elements {
+        display: flex;
+        margin: 0.8rem;
+        align-items: center;
+    }
+
+    .header-search__prefix:active, .header-search__suffix:active {
+        transform: scale(0.8);
+    }
+
+    .header-search__input {
+        width: 30rem;
+        height: 100%;
+        border: none;
+        background-color: transparent;
+        margin: 0 1rem;
+        font-family: var(--font-family);
+        font-weight: 500;
+        color: var(--dark);
+    }
+
+    .header-search__input::placeholder {
+        color: var(--dark)
+    }
+
+    .header-profile {
+        width: 160px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        color: var(--primary);
+        font-size: calc(var(--header-elements) * 0.25);
+    }
+
+    .header-profile > span {
+        font-size: 1.2rem;
+        font-weight: 800;
+    }
+
+    .main-menu-toggle {
+        font-size: calc(var(--header-elements) * 0.16);
+        color: var(--primary);
+        display: none;
+    }
+
+    .main-menu-toggle:active {
+        font-size: calc(var(--header-elements) * 0.1);
+    }
+
+    .header-profile.small, .header-search.small {
+        display: none;
+    }
+
+
+    @media (max-width: 960px) {
+        .header-search.length, .header-profile.length {
+            display: none;
+        }
+
+        .header-search.small, .header-profile.small {
+            display: flex;
+        }
+
+        .header-profile.small {
+            width: 100%;
+            font-size: 3rem;
+            display: flex;
+            flex-flow: column;
+            justify-content: center;
+        }
+
+        .header-search__input {
+            width: 100%;
+        }
+
+        .header-search__elements {
+            width: 100%;
+        }
+
+        .main-nav {
+            position: fixed;
+            height: calc(100vh - 4.8rem);
+            width: 70vw;
+            background: transparent;
+            box-shadow: -4px 10px 4px var(--dark-shadow);
+            top: 4.3rem;
+            right: -80vw;
+            transition: right .3s;
+            padding-top: 1rem;
+            z-index: var(--z-fixed);
+            display: flex;
+            flex-flow: column;
+        }
+
+        .main-nav__block {
+            margin: 20px;
+            display: flex;
+            flex-flow: column;
+            justify-content: flex-end;
+        }
+
+        .main-menu-toggle {
+            display: block;
+        }
+
+    }
+</style>
+
+<header>
+    <div class="main-nav" id="main-nav">
+        <div class="main-nav__block">
+            <div class="header-profile small">
+                <i class="fas fa-user-circle"></i><span>Iniciar sesión</span>
+            </div>
+            <div class="header-search small">
+                <div class="header-search__elements">
+                    <div class="header-search__prefix">
+                        <i class="fas fa-search"></i>
+                    </div>
+                    <input class="header-search__input" type="text" placeholder="Descubre una nueva aventura">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="header-elements">
+        <div class="header-logo">
+            <img class="header-logo__img" src="https://wrixy.com/assets/img/logo.png" alt="Logo Wrixy">
+        </div>
+        <div class="main-menu-toggle" id="main-menu-toggle" on:click={showMenu}><i class="fas fa-bars"></i></div>
+        <div class="header-search length">
+            <div class="header-search__elements">
+                <div class="header-search__prefix">
+                    <i class="fas fa-search"></i>
+                </div>
+                <input class="header-search__input" type="text" placeholder="Descubre una nueva aventura">
+                <div class="header-search__suffix">
+                    <i class="fas fa-filter"></i>
+                </div>
+            </div>
+        </div>
+        <div class="header-profile length">
+            <span>Iniciar sesión</span><i class="fas fa-user-circle"></i>
+        </div>
+    </div>
+</header>
